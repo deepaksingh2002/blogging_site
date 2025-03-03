aea4a4407b202098510101b111fe64d0d919b7c3
Deepak Singh <157193532+deepaksingh2002@users.noreply.github.com>
2025-03-03 14:22:10 +0530

    Update auth.js

--- src/appwrtie/auth.js
+++ src/appwrtie/auth.js
@@ -46,7 +46,7 @@ export class AuthService {
 
     async logout() {
         try {
-            return await this.account.deleteSession(ID);
+            return await this.account.deleteSessions();
         } catch (error) {
             console.log("Appwrite serive :: logout :: erroe", error)
         }
