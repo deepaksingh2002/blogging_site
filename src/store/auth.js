d833d9b5dad91ed0607d5adc9d2153a25c6cef30
Deepak Singh <157193532+deepaksingh2002@users.noreply.github.com>
2025-03-03 13:00:57 +0530

    Update auth.js

--- src/store/auth.js
+++ src/store/auth.js
@@ -9,12 +9,10 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         login: (state, action) => {
-            ...state,
             state.status = true;
             state.userData = action.payload.userData;
         },
         logout: (state) => {
-            ...state,
             state.status = false;
             state.userData = null;
         },
